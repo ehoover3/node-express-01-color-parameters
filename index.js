da2d758ce725421f05d14ff1242d3cfe0bffb375
Eric Hoover <ehoover3@yahoo.com>
2022-01-14 13:58:26 -0800

    first commit, added buttons with a tags

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config(); // Get environment variables
+const express = require("express"); // Require needed node modules
+const app = express(); // Initialize application by calling the function returned by the express module
+
+// ROUTES
+
+// White Page
+app.get("/", function (req, res) {
+  res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `);
+});
+
+// Color Page
+app.get("/:color", function (req, res) {
+  let myColor = req.params.color;
+  res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <button>
+                <a href="/red">Red</a>
+            </button>
+            <button>
+                <a href="/green">Green</a>
+            </button>
+            <button>
+                <a href="/blue">Blue</a>
+            </button>
+            <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
+        </body>
+    `);
+});
+
+app.listen(process.env.PORT); // Listen to a port number defined by a local environment variable
---
+++ notes.md
@@ -0,0 +1,17 @@
+# How to run the server 1 time
+
+type "node index.js" in the terminal
+
+# How to let the server update itself
+
+- npm install -g nodemon
+- nodemon
+
+# How to install express
+
+- npm init -y
+- npm install express
+
+# How to install dotenv
+
+- npm install dotenv
